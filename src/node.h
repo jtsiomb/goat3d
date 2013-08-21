19ccdb29c7c2351f3e90fda5b8b2241b338a3d74
John Tsiombikas <nuclear@member.fsf.org>
2013-08-21 04:00:22 +0300

    foo

--- src/node.h
+++ src/node.h
@@ -3,30 +3,19 @@
 
 #include <string>
 #include <vector>
+#include "xform_node.h"
 
-class Node {
+class Node : public XFormNode {
 private:
-	std::string name;
-	Node *parent;
-	std::vector<Node*> children;
+	Object *obj;
 
 public:
 	Node();
 	virtual ~Node();
 
-	virtual void set_name(const char *name);
-	virtual const char *get_name() const;
-
-	virtual void add_child(Node *c);
-	virtual int get_num_children() const;
-
-	virtual Node *get_child(int idx) const;
-	virtual Node *get_child(const char *name) const;
-	virtual Node *get_descendant(const char *name) const;
-
-	virtual Node *get_parent() const;
-	// passing 0 will return the root
-	virtual Node *get_ancestor(const char *name) const;
+	void set_object(Object *obj);
+	Object *get_object();
+	const Object *get_object() const;
 };
 
 #endif	// NODE_H_
