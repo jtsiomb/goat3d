59508fca5efe3bf41d0f48012e3e7a27e933bc21
John Tsiombikas <nuclear@mutantstargoat.com>
2018-12-27 04:45:42 +0200

    some progress

--- src/read.c
+++ src/read.c
@@ -15,14 +15,75 @@ GNU Lesser General Public License for more details.
 You should have received a copy of the GNU Lesser General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
+#include <treestore.h>
+#include "goat3d.h"
 #include "goat3d_impl.h"
+#include "log.h"
+
+struct key {
+	long tm;
+	cgm_vec4 val;
+};
+
+static struct goat3d_material *read_material(struct goat3d *g, struct ts_node *tsmtl);
+static char *read_material_attrib(struct material_attrib *attr, struct ts_node *tsmattr);
+struct goat3d_mesh *read_mesh(struct goat3d *g, struct ts_node *tsmesh);
 
 int g3dimpl_scnload(struct goat3d *g, struct goat3d_io *io)
 {
-	return -1;
+	struct ts_io tsio;
+	struct ts_node *tsroot, *c;
+
+	tsio.data = io->cls;
+	tsio.read = io->read;
+	tsio.write = io->write;
+
+	if(!(tsroot = ts_load_io(&tsio))) {
+		logmsg(LOG_ERROR, "failed to load scene\n");
+		return -1;
+	}
+	if(strcmp(tsroot->name, "scene") != 0) {
+		logmsg(LOG_ERROR, "invalid scene file, root node is not \"scene\"\n");
+		ts_free_tree(tsroot);
+		return -1;
+	}
+
+	/* read all materials */
+	c = tsroot->children;
+	while(c) {
+		if(strcmp(c->name, "mtl") == 0) {
+			struct goat3d_material *mtl = read_material(g, c);
+			if(mtl) {
+				goat3d_add_mtl(g, mtl);
+			}
+		}
+		c = c->next;
+	}
+
+	/* read all meshes */
+	c = tsroot->children;
+	while(c) {
+		if(strcmp(c->name, "mesh") == 0) {
+			struct goat3d_mesh *mesh = read_mesh(g, c);
+			if(mesh) {
+				goat3d_add_mesh(g, mesh);
+			}
+		}
+		c = c->next;
+	}
+
+	/* read all nodes */
+	/* TODO */
+
+	ts_free_tree(tsroot);
+	return 0;
 }
 
 int g3dimpl_anmload(struct goat3d *g, struct goat3d_io *io)
 {
+	struct ts_io tsio;
+	tsio.data = io->cls;
+	tsio.read = io->read;
+	tsio.write = io->write;
 	return -1;
 }
